93cc24926073a3bcdce048fa917c6fd5826e0e70
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2025-10-02 11:46:30 +0700

    konek ke firebase

--- app/src/main/java/com/example/smarthome/HomeActivity.kt
+++ app/src/main/java/com/example/smarthome/HomeActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.smarthome.databinding.ActivityHomeBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import org.w3c.dom.Text
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private var doubleBackToExitPressedOnce = false
+
+    private lateinit var firebase:FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,19 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        firebase = FirebaseDatabase.getInstance()
+        firebase.reference.child("data").addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val textView = findViewById<TextView>(R.id.greetingText)
+                textView.text = snapshot.value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 
         /* ------------------------ fungsional untuk tombol berpidah activity -------------------------- */
         binding.cardLampu.setOnClickListener {
@@ -66,4 +87,5 @@ class HomeActivity : AppCompatActivity() {
             doubleBackToExitPressedOnce = false
         }, 2000) // Reset after 2 seconds
     }
+
 }
